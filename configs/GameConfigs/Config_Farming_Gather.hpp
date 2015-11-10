ec664fd1559ee50d7cc39c3536bd59cfe8276053
Benancona <bennziwasogo@gmail.com>
2015-11-10 16:39:24 +0100

    gather

--- configs/GameConfigs/Config_Farming_Gather.hpp
+++ configs/GameConfigs/Config_Farming_Gather.hpp
@@ -30,128 +30,112 @@ class ALYSIA_FARMING_GATHER
 		extra[] = {{"artefact_exrare", 1}, {"artefact_rare", 4}, {"artefact_common", 1}};
 		tool = "Skyline_Pioche_01";
 		sound = "mining";
+		extra_all = 1;
 	};
 
-	/*
 	class iron_1
 	{
-		receive = "iron";
-		amount = 2;
-		required = "Skyline_Pioche_01";
+		receive[] = {{"iron", 1, 0}};
+		tool = "Skyline_Pioche_01";
 		sound = "mining";
 	};
 
 	class sand_1
 	{
-		receive = "sand";
-		amount = 5;
-		required = "Skyline_Pelle_01";
+		receive[] = {{"sand", 1, 0}};
+		tool = "Skyline_Pelle_01";
 		sound = "shovel";
 	};
 
 	class oil_1
 	{
-		receive = "oilu";
-		amount = 1;
-		required = "Skyline_Pioche_01";
+		receive[] = {{"oilu", 1, 0}};
+		tool = "Skyline_Pioche_01";
 		sound = "oil";
 	};
 
 	class archeologie_1
 	{
-		receive = "artefact";
-		amount = 1;
-		required = "Skyline_Pioche_01";
+		receive[] = {{"artefact", 1, 0}};
+		tool = "Skyline_Pioche_01";
 		sound = "mining";
 	};
 
 	class prunes_1
 	{
-		receive = "prune";
-		amount = 3;
-		required = "";
+		receive[] = {{"prune", 1, 0}};
+		tool = "";
 		sound = "gather1";
 	};
 	class prunes_2 : prunes_1 {};
 
 	class vignes_1
 	{
-		receive = "raisin";
-		amount = 2;
-		required = "";
+		receive[] = {{"raisin", 0, 0}};
+		tool = "";
 		sound = "gather1";
 	};
 
 	class pillage_1
 	{
-		receive = "objvaleur";
-		amount = 2;
-		required = "";
+		receive[] = {{"objvaleur", 1, 0}};
+		tool = "";
 		sound = "mining";
 	};
-	class pillage_2 : pillage_1 {};
 
 	class plat_1
 	{
-		receive = "platine";
-		amount = 1;
-		required = "Skyline_Merlin_01";
+		receive[] = {{"platine", 1, 0}};
+		tool = "Skyline_Merlin_01";
 		sound = "mining";
 	};
 
 	class gold_1
 	{
-		receive = "gold";
-		amount = 1;
-		required = "Skyline_Pioche_01";
+		receive[] = {{"gold", 1, 0}};
+		tool = "Skyline_Pioche_01";
 		sound = "mining";
 	};
 
 	class clay_1
 	{
-		receive = "clay";
-		amount = 1;
-		required = "Skyline_Pioche_01";
+		receive[] = {{"clay", 1, 0}};
+		tool = "Skyline_Pioche_01";
 		sound = "mining";
 	};
 
 	class coal_1
 	{
-		receive = "coal";
-		amount = 1;
-		required = "Skyline_Pioche_01";
+		receive[] = {{"coal", 1, 0}};
+		tool = "Skyline_Pioche_01";
 		sound = "mining";
 	};
 
 	class salt_1
 	{
-		receive = "salt";
-		amount = 1;
-		required = "Skyline_Pelle_01";
+		receive[] = {{"salt", 1, 0}};
+		tool = "Skyline_Pelle_01";
 		sound = "shovel";
 	};
 
 	class baux_1
 	{
-		receive = "baux";
-		amount = 1;
-		required = "Skyline_Pioche_01";
+		receive[] = {{"baux", 1, 0}};
+		tool = "Skyline_Pioche_01";
 		sound = "mining";
 	};
 
 	class shell_1
 	{
-		receive = "";
-		amount = 1;
-		required = "";
+		receive[] = {{"shell", 1, 0}};
+		tool = "";
 		sound = "";
 	};
 
 	class fish_1
 	{
-		receive = "";
-		amount = 1;
-		required = "";
+		receive[] = {{"", 1, 0}};
+		tool = "";
 		sound = "";
-	};*/
+	};
 };
