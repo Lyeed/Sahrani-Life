782adb0ce5efb9f498f5d52b708154ddb2f2b9b5
DanFett <waylanderdeblore@gmail.com>
2016-01-23 10:24:48 +0100

    chairs

--- configs/GameConfigs/Config_Chairs.hpp
+++ configs/GameConfigs/Config_Chairs.hpp
@@ -24,4 +24,113 @@ class ALYSIA_CHAIRS
 	class xcam_chair_2 {
 		height = 0;
 	};
+	class xcam_Bench_F {
+		height = 0;
+	};
+	class xcam_ChairPlastic_F {
+		height = 0;
+	};
+	class xcam_ChairWood_F {
+		height = 0;
+	};
+	class xcam_Airport_Bench {
+		height = 0;
+	};
+	class xcam_ch_mod_c {
+		height = 0;
+	};
+	class xcam_kitchen_chair_a {
+		height = 0;
+	};
+	class xcam_ch_mod_h {
+		height = 0;
+	};
+	class xcam_ch_office_B {
+		height = 0;
+	};
+	class xcam_Lobby_Chair {
+		height = 0;
+	};
+	class xcam_hospital_bench {
+		height = 0;
+	};
+	class xcam_bench_EP1 {
+		height = 0;
+	};
+	class xcam_chair_EP1 {
+		height = 0;
+	};
+	class xcam_chair {
+		height = 0;
+	};
+	class xcam_armchair {
+		height = 0;
+	};
+	class xcam_ch_mod_d {
+		height = 0;
+	};
+	class xcam_ch_mod_e {
+		height = 0;
+	};
+	class xcam_chair_cargo {
+		height = 0;
+	};
+	class Bleacher_EP1 {
+		height = 0;
+	};
+	class Land_Bench_F {
+		height = 0;
+	};
+	class Land_ChairPlastic_F {
+		height = 0;
+	};
+	class FoldChair {
+		height = 0;
+	};
+	class Park_bench1 {
+		height = 0;
+	};
+	class Park_bench2 {
+		height = 0;
+	};
+	class Park_bench2_noRoad {
+		height = 0;
+	};
+	class Land_Bench_EP1 {
+		height = 0;
+	};
+	class xcam_CampingChair_V1_F {
+		height = 0;
+	};
+	class xcam_CampingChair_V2_F {
+		height = 0;
+	};
+	class xcam_Tribune_F {
+		height = 0;
+	};
+	class xcam_WoodenLog_F {
+		height = 0;
+	};
+	class xcam_Bench_02_F {
+		height = 0;
+	};
+	class xcam_lavicka_3 {
+		height = 0;
+	};
+	class xcam_lavicka_4 {
+		height = 0;
+	};
+	class xcam_lavicka_4bezroadwaye {
+		height = 0;
+	};
+	class xcam_lavicka_1 {
+		height = 0;
+	};
+	class xcam_lavicka_2 {
+		height = 0;
+	};
+	class xcam_bleacher {
+		height = 0;
+	};
+	
 };
